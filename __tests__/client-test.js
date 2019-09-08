03db0b55668303d6377d6d370d9e9f67c2494d00
Justin Richards <richards.justind@gmail.com>
2019-09-08 14:34:41 -0400

    add supertest-fetch to front-end script when loading for tests

--- __tests__/client-test.js
+++ __tests__/client-test.js
@@ -1,11 +1,38 @@
-// const app = require("../app");
+const app = require("../src/app");
 
-// The assertion for a promise must be returned.
-describe("proxy api", () => {
-    it("will do something", () => {
-        expect.assertions(1);
-        // return user.getUserName(4).then(data => expect(data).toEqual('Mark'));
-        return expect(1).toEqual(1);
-    });
+const $ = sel => document.querySelector(sel);
+
+test("Submitting a valid query will populate the \
+books-list within 5 seconds.", done => {
+    // arrange
+    const bookList = $(".book-list");
+
+    // act
+    $("button").click();
+    expect(document.querySelector(".book-list").innerHTML).toEqual("foo");
+
+    // assert
+        // (since we don't have separate modules to test, it's tricky to 
+            // test an event with an async handler. we will just check
+            // for a render every half second, up to a maximum of 5 seconds)
+
+    const now = new Date();
+    const delay = 500;
+    const checkForRender = () => {
+        if (bookList.innerHTML === "") {
+            if (new Date() - now > 5000) {
+                expect(bookList.querySelector("li")).not.toBeNull()
+            }
+            setTimeout(checkForRender, delay)
+        }
+        else {
+            expect(bookList.querySelector("li")).not.toBeNull()
+        }
+    }
 })
   
+
+// test("Submitting a query with no results will lead to a no-results \
+// message in the books-list", () => {
+
+// })
--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
+// if we are testing, substitute supertest-compatible fetch for browser's fetch
+if (typeof process !== "undefined" && process.env.NODE_ENV === "test") {
+    const http = require("http");
+    const { makeFetch } = require("supertest-fetch");
+    fetch = makeFetch(http.createServer(require("../app")));
+}
+
 // SETTINGS
 SEARCH_URL = "/book-search";
 
@@ -11,16 +18,17 @@ const formatQueryURL = (baseURL, query) => `${baseURL}?q=${query}`;
 // ACTIONS
 const fetchBooks = query => {
     fetch(formatQueryURL(SEARCH_URL, query))
-        .catch( 
-            err => console.error(err)
+        .then(
+            resp => resp.json(),
+            err => console.error("error reading response", err)
         )
-        .then(resp => resp.json())
-        .then(json => console.log(json));
+        .then(
+            renderBooksResponse,
+            err => console.error("error reading json", err)
+        );
 }
 
-
 // VIEW LOGIC
-
 // (writing the document body here instead of in the index file so that the 
     // same html is created when requiring this module for testing)
 document.body.innerHTML = `
@@ -28,19 +36,40 @@ document.body.innerHTML = `
     <i>Shhh!</i>
     <p>florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum florum clorum borum decorum </p>
     <form class="search-form">
-        <input class="search-input" id="searchInput" />
-        <button type="submit" />
+        <input class="search-input" id="searchInput" name="searchInput" />
+        <button type="submit"></button>
     </form>
-    <ul class="book-list">
-    </ul>
+    <ul class="book-list"></ul>
 `;
 
 const handleFormSubmit = e => {
     e.preventDefault();
-    window.trgt = e.target;
-    $(".book-list").innerHTML = "poo";
+    fetchBooks($("#searchInput").value);
 }
 
+const renderBooksResponse = json => {
+    $(".book-list").innerHTML = json.items.map(renderBookItem).join("")
+}
+
+const renderBookItem = bookObj => `
+    <li class="book-item">
+        <h4 class="book-title">Title: ${bookObj.volumeInfo.title}</h4>
+        <p class="book-authors">Authors: ${
+            bookObj.volumeInfo.authors ? 
+            bookObj.volumeInfo.authors.join(", ") :
+            "N/A"
+        }
+        </p>
+        <p class="publishing company">Publisher: ${bookObj.volumeInfo.publisher}</p>
+        ${
+            bookObj.volumeInfo.imageLinks ? 
+            `<img src="${bookObj.volumeInfo.imageLinks.smallThumbnail}" />`  :
+            ""
+        }
+        />
+    </li>
+`
+
+// assign event listeners
 $(".search-form").addEventListener("submit", handleFormSubmit);
 
-fetchBooks("dinosaurs");
